8c6da1fd36378ff34e4c4c540120a9ffcdc2b055
skoh <skoh83@yahoo.co.kr>
2016-04-21 23:37:11 +0900

    1

---
+++ common-web/install_jar.bat
@@ -0,0 +1,13 @@
+set MAVEN_HOME=C:/dev/lib/apache-maven-3.3.9
+
+e:
+cd \git\skoh-nemus\s-lbs-server\common-libs
+REM cd \dev\git\skoh-nemus\s-lbs-server\common-libs
+
+REM %MAVEN_HOME%/bin/mvn.cmd install:install-file -Dfile=../common-libs/aams_vo-1.0.jar -DgroupId=com.ssg.platform.lbs.aams.vo -DartifactId=aams_vo -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
+REM %MAVEN_HOME%/bin/mvn.cmd install:install-file -Dfile=../common-libs/bms_vo-1.0.jar -DgroupId=com.ssg.platform.lbs.bms.vo -DartifactId=bms_vo -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
+REM %MAVEN_HOME%/bin/mvn.cmd install:install-file -Dfile=../common-libs/cbms_vo-1.0.jar -DgroupId=com.ssg.platform.lbs.cbms.vo -DartifactId=cbms_vo -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
+REM %MAVEN_HOME%/bin/mvn.cmd install:install-file -Dfile=../common-libs/common-web-1.0.jar -DgroupId=org.oh.common -DartifactId=common-web -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
+REM %MAVEN_HOME%/bin/mvn.cmd install:install-file -Dfile=../common-libs/devms-tokenizer-1.0.0-SNAPSHOT-jar-with-dependencies.jar -DgroupId=com.ssg.platform.lbs.devms.tokenizer -DartifactId=devms_tokenizer -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
+REM %MAVEN_HOME%/bin/mvn.cmd install:install-file -Dfile=../common-libs/mms_vo-1.0.jar -DgroupId=com.ssg.platform.lbs.mms.vo -DartifactId=mms_vo -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
+REM %MAVEN_HOME%/bin/mvn.cmd install:install-file -Dfile=../common-libs/zms_model-1.0.jar -DgroupId=com.ssg.platform.lbs.zms.model -DartifactId=zms_model -Dversion=1.0 -Dpackaging=jar -DgeneratePom=true
--- common-web/src/test/java/org/oh/common/TestAPI_skoh.java
+++ common-web/src/test/java/org/oh/common/TestAPI_skoh.java
@@ -29,8 +29,8 @@ public class TestAPI_skoh extends TestAPI {
 //		arrayNode.add(readFile("src/test/resources/json/sample_list2_get.json"));
 //		arrayNode.add(readFile("src/test/resources/json/sample_list_get.json"));
 //		arrayNode.add(readFile("src/test/resources/json/sample_list2_get.json"));
-		arrayNode.add(readFile("src/test/resources/json/sample_page_post.json"));
-		arrayNode.add(readFile("src/test/resources/json/sample_page2_post.json"));
+//		arrayNode.add(readFile("src/test/resources/json/sample_page_post.json"));
+//		arrayNode.add(readFile("src/test/resources/json/sample_page2_post.json"));
 //		arrayNode.add(readFile("src/test/resources/json/sample_update_post.json"));
 //		arrayNode.add(readFile("src/test/resources/json/sample_update2_post.json"));
 
@@ -51,6 +51,7 @@ public class TestAPI_skoh extends TestAPI {
 //		arrayNode.add(readFile("src/test/resources/json/zms_search_get.json"));
 //		arrayNode.add(readFile("src/test/resources/json/zms_search2_get.json"));
 //		arrayNode.add(readFile("src/test/resources/json/zms_search2_get2.json"));
+//		arrayNode.add(readFile("src/test/resources/json/zms_search2_get3.json"));
 //		arrayNode.add(readFile("src/test/resources/json/zms_searchall_get.json"));
 
 //		arrayNode.add(readFile("src/test/resources/json/bms_beacon_put.json"));

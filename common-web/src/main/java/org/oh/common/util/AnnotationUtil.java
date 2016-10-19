2ddb83bf91b8b47dbfb636c0b9051def5bbd4dd3
skoh <skoh83@yahoo.co.kr>
2016-10-20 02:41:09 +0900

    1

--- common-web/src/main/java/org/oh/common/util/AnnotationUtil.java
+++ common-web/src/main/java/org/oh/common/util/AnnotationUtil.java
@@ -6,6 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.reflect.Field;
 import java.util.Map;
 
+import org.mybatisorm.annotation.Table;
 import org.springframework.core.annotation.AnnotationUtils;
 
 public abstract class AnnotationUtil extends AnnotationUtils {
@@ -19,34 +20,36 @@ public abstract class AnnotationUtil extends AnnotationUtils {
 	}
 
 	@Retention(RetentionPolicy.RUNTIME)
-	@interface Something {
-		String someProperty();
+	public @interface Something {
+		String value() default "";
+//		int number() default 1;
 	}
 
-	@Something(someProperty = "some")
+	@Something("some")
+//	@Table
 	public class Foobar {
 	}
 
 	public static void main(String[] args) throws Exception {
 //		System.out.println(isAnnotationDeclaredLocally(Component.class, Controller.class));
 
-		final Something annotation = (Something) Foobar.class.getAnnotations()[0];
-		System.out.println(annotation.someProperty());
+		final Annotation[] annotation = Foobar.class.getAnnotations();
+		System.out.println(Utils.toString(annotation));
 
 		Annotation newAnnotation = new Something() {
 			@Override
-			public String someProperty() {
+			public String value() {
 				return "another";
 			}
 
 			@Override
 			public Class<? extends Annotation> annotationType() {
-				return annotation.annotationType();
+				return annotation[0].annotationType();
 			}
 		};
 		setAnnotation(newAnnotation);
 
-		Something annotation2 = (Something) Foobar.class.getAnnotations()[0];
-		System.out.println(annotation2.someProperty());
+		Annotation[] annotation2 = Foobar.class.getAnnotations();
+		System.out.println(Utils.toString(((Something) annotation2[0]).value()));
 	}
 }

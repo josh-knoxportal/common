a743f7ecd1042351a28664c918126ea2d9b5ad44
오성균 <skoh@nemustech.com>
2016-11-16 00:42:07 +0900

    no message

--- common-web/src/test/java/com/nemustech/common/TestAPI_ssg.java
+++ common-web/src/test/java/com/nemustech/common/TestAPI_ssg.java
@@ -127,7 +127,7 @@ public class TestAPI_ssg extends TestAPI {
 //		arrayNode.add(readFile("src/test/resources/json/ssg/zms/category_delete_delete.json"));
 //		arrayNode.add(readFile("src/test/resources/json/ssg/zms/category_insert_list_post.json"));
 //		arrayNode.add(readFile("src/test/resources/json/ssg/zms/category_select_post.json"));
-		arrayNode.add(readFile("src/test/resources/json/ssg/zms/category_select_post2.json"));
+//		arrayNode.add(readFile("src/test/resources/json/ssg/zms/category_select_post2.json"));
 //		arrayNode.add(readFile("src/test/resources/json/ssg/zms/geozone_get.json"));
 //		arrayNode.add(readFile("src/test/resources/json/ssg/zms/geozone_around_get.json"));
 //		arrayNode.add(readFile("src/test/resources/json/ssg/zms/geozone_id_around_get.json"));

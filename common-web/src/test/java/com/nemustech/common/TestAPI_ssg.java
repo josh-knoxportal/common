7da3ff3b8036284bfe60403c0112961c88f8b239
오성균 <skoh@nemustech.com>
2016-11-23 10:12:57 +0900

    no message

--- common-web/src/test/java/com/nemustech/common/TestAPI_ssg.java
+++ common-web/src/test/java/com/nemustech/common/TestAPI_ssg.java
@@ -81,14 +81,14 @@ public class TestAPI_ssg extends TestAPI {
 //		arrayNode.add(readFile("src/test/resources/json/ssg/lms/daily_zone_visit_log_dashboard_get.json"));
 //		arrayNode.add(readFile("src/test/resources/json/ssg/lms/hourly_zone_visit_log_group_list_get.json"));
 //		arrayNode.add(readFile("src/test/resources/json/ssg/lms/make_route_csv_get.json"));
-//		arrayNode.add(readFile("src/test/resources/json/ssg/lms/route_post.json"));
+		arrayNode.add(readFile("src/test/resources/json/ssg/lms/route_post.json"));
 //		arrayNode.add(readFile("src/test/resources/json/ssg/lms/route_post2.json"));
 //		arrayNode.add(readFile("src/test/resources/json/ssg/lms/route_post3.json"));
 //		arrayNode.add(readFile("src/test/resources/json/ssg/lms/verify_insert_post.json"));
 //		arrayNode.add(readFile("src/test/resources/json/ssg/lms/verify_inserts_json_post.json"));
 //		arrayNode.add(readFile("src/test/resources/json/ssg/lms/verify_updates_json_post.json"));
 //		arrayNode.add(readFile("src/test/resources/json/ssg/lms/verify_deletes_json_post.json"));
-		arrayNode.add(readFile("src/test/resources/json/ssg/lms/verify_list_get.json"));
+//		arrayNode.add(readFile("src/test/resources/json/ssg/lms/verify_list_get.json"));
 //		arrayNode.add(readFile("src/test/resources/json/ssg/lms/verify_select_get.json"));
 //		arrayNode.add(readFile("src/test/resources/json/ssg/lms/verify_select_get2.json"));
 //		arrayNode.add(readFile("src/test/resources/json/ssg/lms/verify_select_get3.json"));
